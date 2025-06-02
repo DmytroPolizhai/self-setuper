fff3a6bb4449301d2149d7525d7f4dcbc9748189
DmytroPolizhai <dmytropolizhai@gmail.com>
2025-06-03 01:04:48 +0300

    Project init. Working with logic base 🚀🔥
    Signed-off-by: DmytroPolizhai <dmytropolizhai@gmail.com>

---
+++ src/utils/FileCreator.ts
@@ -0,0 +1,13 @@
+﻿import * as fs from 'fs';
+
+
+export class FileCreator {
+    async createBatFile(id: string, date: string, filling: string) {
+        fs.writeFile(
+            `@/assets/temp/${id}-${date}.bat`,
+            filling,
+            (err) => {
+                if (err) throw err;
+            })
+    }
+}
---
+++ src/utils/FileIdManager.ts
@@ -0,0 +1,15 @@
+﻿export const FileIdManager = {
+    currentIds: new Set<number>(),
+
+    add(id: number) {
+        this.currentIds.add(id);
+    },
+
+    getFreeId(): number {
+        let id = 1;
+        while (this.currentIds.has(id)) {
+            id++;
+        }
+        return id;
+    },
+};
