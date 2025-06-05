bdfc4ff8f1b1cdad2ae9d37964aa1b2b328a35d9
DmytroPolizhai <dmytropolizhai@gmail.com>
2025-06-05 18:24:04 +0300

    Some features are here: made core of the backend.

---
+++ backend/src/cli.py
@@ -0,0 +1,20 @@
+﻿import argparse
+
+from server import Server
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Start API service.")
+
+    parser.add_argument("--root_endpoint", action="store", type=str, default="api", help="Root endpoint(default api)")
+    parser.add_argument("--port", action="store", type=int, default=8000, help="Server port number(default 8000)")
+    parser.add_argument("--host", action="store", type=str, default="localhost", help="Server host(default localhost)")
+
+    args = parser.parse_args()
+
+    server = Server(root_endpoint=args.root_endpoint, host=args.host, port=args.port)
+    server.start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ backend/src/server.py
@@ -0,0 +1,23 @@
+﻿from fastapi import FastAPI
+import uvicorn
+
+"""
+Server class for managing API of frontend app
+"""
+class Server:
+    def __init__(self, root_endpoint: str = "api", host: str = "localhost", port: int = 8000) -> None:
+        self._app = FastAPI()
+        self._root_endpoint = root_endpoint
+        self._host = host
+        self._port = port
+
+        @self._app.get(f"/{self._root_endpoint}")
+        def read_root():
+            return {"ping": "pong!"}
+
+    def start(self) -> None:
+        host = self._host
+        port = self._port
+
+        print(f"Server started\nhttp://{host}:{port}/{self._root_endpoint}. Press Ctrl+C to exit.")
+        uvicorn.run(self._app, host=host, port=port)
