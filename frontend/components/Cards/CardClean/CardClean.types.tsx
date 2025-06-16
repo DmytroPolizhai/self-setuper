9661804b7d4c0e43a8379c29ace5d19c8527f25b
Dmytro <dmytropolizhai@gmail.com>
2025-06-16 08:28:35 +0300

    * Created CustomScrollView.tsx. * Created CardViewer.tsx for controlling AppCard.tsx. * Created LoadingIndicator.tsx. * Created CardList.tsx. * Components is sorted by folders. * Created CardView.tsx. * Created utils folder in shared. * Created array.ts * Created storage.ts
    Work: deleting.
    Doesn't work: editing, downloading, uploading an image.
    
    Signed-off-by: Dmytro <dmytropolizhai@gmail.com>

---
+++ frontend/components/Cards/CardClean/CardClean.types.tsx
@@ -0,0 +1,8 @@
+﻿export type CardCleanTypes = {
+    title: string,
+    description?: string,
+    imageUri?: string,
+    onDelete?: () => void,
+    onEdit?: () => void,
+    onUploadImage?: () => void,
+}
